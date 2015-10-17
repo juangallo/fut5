df6f9d4b12cb303aba7c56a3385d84af0f6a3495
Mikael Lijtenstein <mikael_lij@hotmail.com>
2015-10-17 19:28:22 -0300

    Registro de usuario

--- app/src/main/java/com/ort/num172159_180968/fut5/Register.java
+++ app/src/main/java/com/ort/num172159_180968/fut5/Register.java
@@ -1,50 +1,51 @@
 package com.ort.num172159_180968.fut5;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 
-public class LogIn extends AppCompatActivity {
+public class Register extends AppCompatActivity {
 
+    Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
+        setContentView(R.layout.activity_register);
 
-        Button btnOk = (Button)findViewById(R.id.btnOk);
-        btnOk.setOnClickListener(new View.OnClickListener() {
+        btnSaveClick();
 
+
+    }
+
+    public void btnSaveClick(){
+
+        btnSave = (Button)findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), MainMenu.class);
-                startActivityForResult(intent, 0);
+                String name = findViewById(R.id.txtPersonName).toString();
+                String email = findViewById(R.id.txtEmailAddress).toString();
+                String username = findViewById(R.id.txtUserNameRegister).toString();
+                String password = findViewById(R.id.txtPasswordRegister).toString();
+
+                //llamar a la api para insertar usuario
+
+                //si esta ok guardarlo y mandarlo al login para que entre
+
+                
+
             }
         });
-
-        Button btnRegister = (Button)findViewById(R.id.btnRegister);
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), Register.class);
-                startActivityForResult(intent, 0);
-            }
-        });
-
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_log_in, menu);
+        getMenuInflater().inflate(R.menu.menu_register, menu);
         return true;
     }
 
