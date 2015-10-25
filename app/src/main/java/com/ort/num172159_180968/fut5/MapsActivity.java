de7b35e7d04abb30fc298612caf1b1c3f4d9256c
Juan Martin Gallo <juan_gallo1@hotmail.com>
2015-10-25 00:26:00 -0300

    new ip, and changes on maps activity

--- app/src/main/java/com/ort/num172159_180968/fut5/MapsActivity.java
+++ app/src/main/java/com/ort/num172159_180968/fut5/MapsActivity.java
@@ -2,14 +2,19 @@ package com.ort.num172159_180968.fut5;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.Display;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.magnet.android.mms.MagnetMobileClient;
 import com.magnet.android.mms.async.Call;
@@ -17,6 +22,7 @@ import com.ort.num172159_180968.fut5.controller.api.Fields;
 import com.ort.num172159_180968.fut5.controller.api.FieldsFactory;
 import com.ort.num172159_180968.fut5.model.beans.FieldsResult;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -24,6 +30,7 @@ public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private Fields fields;
+    private List<Marker> markers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +38,8 @@ public class MapsActivity extends FragmentActivity {
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
         mMap.setMyLocationEnabled(true);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-34.8076549, -56.1802311), 11));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-34.8076549, -56.1802311), 11));
+        markers = new ArrayList<>();
     }
 
     @Override
@@ -78,7 +86,8 @@ public class MapsActivity extends FragmentActivity {
                 Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.soccerfieldmarker);
                 Bitmap newBitmap = Bitmap.createScaledBitmap(bm, 40, 40, true);
                 for (FieldsResult field : result) {
-                    mMap.addMarker(new MarkerOptions().position(new LatLng(field.getFieldLat(), field.getFieldLon())).title(field.getFieldName()).icon(BitmapDescriptorFactory.fromBitmap(newBitmap)));
+                    Marker marker = mMap.addMarker(new MarkerOptions().position(new LatLng(field.getFieldLat(), field.getFieldLon())).title(field.getFieldName()).icon(BitmapDescriptorFactory.fromBitmap(newBitmap)));
+                    markers.add(marker);
                 }
 
             } catch (InterruptedException e) {
@@ -87,6 +96,23 @@ public class MapsActivity extends FragmentActivity {
                 e.printStackTrace();
             }
         }
+        centerMap();
         //int distance =
+    }
+
+    public void centerMap(){
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        for(Marker m : markers){
+            builder.include(m.getPosition());
+        }
+        LatLngBounds bounds = builder.build();
+        int padding = 0; // offset from edges of the map in pixels
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, width, height, padding);
+        mMap.animateCamera(cu);
     }
 }
--- app/src/main/java/com/ort/num172159_180968/fut5/controller/api/Value.java
+++ app/src/main/java/com/ort/num172159_180968/fut5/controller/api/Value.java
@@ -4,5 +4,5 @@ package com.ort.num172159_180968.fut5.controller.api;
  * Created by JuanMartin on 23-Oct-15.
  */
 public class Value {
-    protected static String ip = "192.168.0.129";
+    protected static String ip = "192.168.1.102";
 }
